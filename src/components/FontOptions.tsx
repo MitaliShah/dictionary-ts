398a029eec0ab3422e1a8ac9295753eb856e9b0a
MitaliShah <mitalishaleenshah@gmail.com>
2024-06-02 07:22:17 -0500

    feat: custom dropdown and styling with react-select

--- src/components/FontOptions.tsx
+++ src/components/FontOptions.tsx
@@ -1,59 +1,75 @@
-import { useEffect } from 'react';
-import { useFontThemeContext } from '../FontThemeContext';
-import styled from 'styled-components';
+import { useEffect } from "react";
+import { useFontThemeContext } from "../FontThemeContext";
+import Select, { StylesConfig } from "react-select";
 
-export default function FontOptions() {
-    const { selectedFont, setSelectedFont } = useFontThemeContext();
+const options = [
+  { value: "serif", label: "Serif" },
+  { value: "sans-serif", label: "Sans Serif" },
+  { value: "monospace", label: "Monospace" },
+];
 
-    useEffect(() => {
-        document.body.style.fontFamily = selectedFont;
-    }, [selectedFont]);
-    
-    return(
-        <div>
-            <label 
-            htmlFor="font-select"
-            className='visually-hidden'
-            >
-            Select Font
-            </label>
-            
-            <Select            
-            id="font-select"
-            value={selectedFont}
-            onChange={event => {
-                setSelectedFont(event.target.value)
-            }}                       
-            >
-            <option 
-                value="serif"
-                aria-selected={selectedFont === "serif"}
-            >
-                Serif
-            </option>
-            <option 
-                value="sans-serif"
-                aria-selected={selectedFont === "sans-serif"}
-            >
-                Sans Serif
-            </option>
-            <option 
-                value="monospace"
-                aria-selected={selectedFont === "monospace"}
-            >
-                Monospace
-            </option>            
-            </Select>
-        </div>
-    )
+interface OptionType {
+  value: string;
+  label: string;
 }
 
-const Select = styled.select`
-    font-size: 14px;
-    font-weight: bold;
-    background: none;
-    border: none;
-    color: var(--charcoal-gray);
-    outline: none;
-    color: var(--violet);
-`;
+export default function FontOptions() {
+  const { isDarkTheme, selectedFont, setSelectedFont } = useFontThemeContext();
+
+  useEffect(() => {
+    if (!selectedFont) {
+      setSelectedFont(options[0].value);
+    }
+    document.body.style.fontFamily = selectedFont;
+  }, [selectedFont, setSelectedFont]);
+
+  const handleChange = (selectedOption: OptionType | null) => {
+    if (selectedOption) {
+      setSelectedFont(selectedOption.value);
+    }
+  };
+
+  const customStyles: StylesConfig<OptionType, false> = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "none",
+      border: "none",
+      outline: "none",
+      boxShadow: "none",
+      color: isDarkTheme ? "var(--very-dark-gray)" : "var(--white)",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: isDarkTheme ? "var(--white)" : "var(--charcoal-gray)",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: isDarkTheme ? "var(--very-dark-gray)" : "var(--white)",
+      boxShadow: "0 0 5px var(--violet)",
+    }),
+    option: (provided) => ({
+      ...provided,
+      backgroundColor: "inherit",
+      color: isDarkTheme ? "var(--white)" : "var(--charcoal-gray)",
+      "&:hover": {
+        color: "var(--violet)",
+      },
+    }),
+    dropdownIndicator: (base) => ({
+      ...base,
+      color: "var(--violet)",
+      "&:hover": {
+        color: "var(--violet)",
+      },
+    }),
+  };
+
+  return (
+    <Select
+      options={options}
+      onChange={handleChange}
+      styles={customStyles}
+      value={options.find((option) => option.value === selectedFont)}
+    />
+  );
+}
